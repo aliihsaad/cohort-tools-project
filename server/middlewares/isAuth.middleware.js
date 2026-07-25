5ba737bc354bcc0689484df7bcd2913cbc12d6eb
ecommfox <medellint@gmail.com>
2026-07-25 13:19:45 +0100

    Fixed middleware file

--- server/middlewares/isAuth.middleware.js
+++ server/middlewares/isAuth.middleware.js
@@ -12,4 +12,11 @@ export function isAuth(req, res, next) {
     }else { res.status(401).json({message: "Token not provided"}); }
   }catch(error){
     console.log(error);
-    res.staJW
+    res.status(400).json({ message: "Error verifying token" });
+  }
+
+}
+
+
+  
+  
